1d111eb0eb2facc1cb71c6070a196325cc81fcd5
xKoc0 <bautitheone@gmail.com>
2024-03-29 18:09:54 -0300

    Modified some admin settings

--- my_padel_club/members/admin.py
+++ my_padel_club/members/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "phone", "Joined_date")
+
+admin.site.register(Member, MemberAdmin)
--- my_padel_club/members/models.py
+++ my_padel_club/members/models.py
@@ -5,3 +5,7 @@ class Member(models.Model):
     lastname = models.CharField(max_length = 255)
     phone = models.IntegerField(null=True)
     Joined_date = models.DateField(null=True) 
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+    
