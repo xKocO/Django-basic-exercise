a4d5edb0367f166763e67e9db285b60395c2c469
John xkoc0 <bautitheone@gmail.com>
2024-12-11 16:23:28 -0300

    Added global files

--- my_padel_club/my_padel_club/settings.py
+++ my_padel_club/my_padel_club/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 STATIC_ROOT = BASE_DIR / "productionfiles"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
